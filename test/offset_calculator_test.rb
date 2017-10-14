831e7c74c3244e5fcb71748445cd6deec0db2b52
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2017-10-14 13:33:02 -0600

    commit current key instance test

--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -10,4 +10,17 @@ class OffsetCalculatorTest < Minitest::Test
 
     assert_instance_of OffsetCalculator, current_offset
   end
+
+  def test_current_date_is_instance_of_date_class
+    current_offset = OffsetCalculator.new
+
+    assert_instance_of Date, current_offset.current_date
+  end
+
+  def test_current_key_is_instance_of_string_class
+    current_offset = OffsetCalculator.new
+
+    assert_instance_of String, current_offset.current_key
+  end
+
 end
