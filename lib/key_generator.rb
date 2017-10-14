50c28be0563e37b6c1e47949fb407a5a84f6f6b2
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2017-10-14 12:40:42 -0600

    test random key passing

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,5 +1,9 @@
 class KeyGenerator
 
+def key
+  5.times.map {rand(10)}.join
+
+end
 
 
 
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -9,7 +9,20 @@ class KeyGeneratorTest < Minitest::Test
     current_key = KeyGenerator.new
 
     assert_instance_of KeyGenerator, current_key
+  end
 
+  def test_random_key_generated_has_5_digits
+    current_key = KeyGenerator.new
+
+    assert_equal 5,current_key.key.length
+  end
+
+  def test_random_key_generated_includes_digits_0_to_9
+    current_key = KeyGenerator.new
+
+    digits = [0,1,2,3,4,5,6,7,8,9]
+
+    assert digits.any?{|digit| digit = current_key.key[4].to_i}
   end
 
 end
