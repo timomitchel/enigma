45ad60ca18b5c0b8a9b9e4e71bbb0a3979c7faa5
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-15 09:37:50 -0600

    more encryptor functionality that grabs index position of character map

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,5 +1,5 @@
 require_relative 'offset_calculator'
-
+require_relative 'character_map'
 class Encryptor
 
   def initialize(message)
@@ -15,10 +15,8 @@ class Encryptor
   end
 
   def message_splitter
-    @message.chars
+    @message.downcase.chars
   end
-
-
 
   def a_index_finder
     index = 0
@@ -54,8 +52,17 @@ class Encryptor
       index += 3
     end
   end
+
+  def mapper(index)
+        index.map do |char|
+         character_map.index(char)
+    end
+  end
+
+
 end
 
 e = Encryptor.new('Hello there I am a fabulous new message you shit')
 p e.a_index_finder
-p e.b_index_finder
+p e.character_map[0..36]
+p e.mapper(e.a_index_finder)
