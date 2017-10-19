8dbcb68a864773817fa142f474a255b6c543e219
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-18 18:53:38 -0600

    tests passing

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -114,8 +114,66 @@ class DecryptorTest < Minitest::Test
     assert_equal 47, decrypt.offset_d
   end
 
-  def test_case_name
+  def test_character_map_indexes
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+    expected = decrypt.character_map_indexes(decrypt.a_index_finder(0))
 
+    assert_instance_of Array, expected
+    assert_equal [7, 36, 15, 4], expected
+  end
+
+  def test_neg_a_rotator
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+
+    expected = decrypt.neg_a_rotator
+
+    assert_instance_of Array, expected
+    assert_equal ["s", "i", "0", "p"], expected
+  end
+
+  def test_neg_b_rotator
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+
+    expected = decrypt.neg_b_rotator
+
+    assert_instance_of Array, expected
+    assert_equal [" ", "j", "i"], expected
+  end
+
+  def test_neg_c_rotator
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+
+    expected = decrypt.neg_c_rotator
+
+    assert_instance_of Array, expected
+    assert_equal ["x", "q", "m"], expected
+  end
+
+  def test_neg_d_rotator
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+
+    expected = decrypt.neg_d_rotator
+
+    assert_instance_of Array, expected
+    assert_equal ["h", "2", "r"], expected
+  end
+
+  def test_zip_rotated_characters_returns_array_of_arrays
+    decrypt = Decryptor.new('halp me plaze', '24238', "071017")
+    actual = [["s", " ", "x", "h"],
+              ["i", "j", "q", "2"],
+              ["0", "i", "m", "r"],
+              ["p", nil, nil, nil]]
+
+    assert_instance_of Array, decrypt.zip_rotated_characters
+    assert_equal actual, decrypt.zip_rotated_characters
+  end
+
+  def test_format_decrypted_message
+    decrypt = Decryptor.new('du0idn8ea7t7pxxp6y77pxt7iu7p9wt7sum numik4zfjwmqknsb.7amp','31466',"171017")
+
+    assert_instance_of String, decrypt.format_decrypted_message
+    assert_equal 'hellh there this is the message we are looking to decrypt', decrypt.format_decrypted_message
   end
 
 end
